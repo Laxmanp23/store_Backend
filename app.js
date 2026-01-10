e6960f2065392ff2b17770a2b877f965e76b30a9
laxmanp23 <laxmanpawar4675@gmail.com>
2026-01-10 10:15:30 +0530

    Add CI/CD workflow

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3007;
+const port = 9001;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const sequelize = require("./src/config/config.js");
